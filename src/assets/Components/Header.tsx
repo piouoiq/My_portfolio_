50472f573b01f2a40fae84e44afb4520fd9aee13
piouoiq <melargilliere@gmail.com>
2025-12-10 15:52:20 +0100

    install tailwind

---
+++ src/assets/Components/Header.tsx
@@ -0,0 +1,8 @@
+export default function Header() {
+  return (
+    <header className="">
+      <h1>Melissa Largilliere</h1>
+      <p>Student developer at Ada Tech School</p>
+    </header>
+  );
+}
