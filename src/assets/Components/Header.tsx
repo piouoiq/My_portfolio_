86144af828b5c592f3628fd66b4cd60b05b74954
piouoiq <melargilliere@gmail.com>
2025-12-10 16:05:50 +0100

    bg color

--- src/assets/Components/Header.tsx
+++ src/assets/Components/Header.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="">
+    <header className="bg-[#ee8f15] p-4 text-center">
       <h1>Melissa Largilliere</h1>
       <p>Student developer at Ada Tech School</p>
     </header>
