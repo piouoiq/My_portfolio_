42d5d5ba9958d608a9b87160faadf46431cd21e7
piouoiq <melargilliere@gmail.com>
2025-12-13 00:01:13 +0100

    fix header underline

--- src/assets/Components/Header.tsx
+++ src/assets/Components/Header.tsx
@@ -1,7 +1,7 @@
 export default function Header() {
   return (
     <header className="font-gruppo font-bold  bg-[#ee8f15] p-4 text-center position-sticky top-0">
-      <h1 className=" text-4xl">Melissa Largilliere</h1>
+      <h1 className=" text-4xl underline">Melissa Largilliere</h1>
       <p className="text-xl">Student developer at Ada Tech School</p>
     </header>
   );
