3d1d07fef73f8b5c26c22ee6d523c3e3d8e61a20
piouoiq <melargilliere@gmail.com>
2025-12-14 17:13:04 +0100

    feat new project

--- src/Data/Projects.ts
+++ src/Data/Projects.ts
@@ -2,15 +2,22 @@ import type { Project } from "../Types/Project";
 
 export const projects: Project[] = [
   {
-    id: "transcrimorse",
-    title: "TranscriMorse",
+    id: "Dataviz",
+    title: "Dataviz",
     description:
-      "A website that transcript Morse-code to text and the other way around.",
-    image: "src/assets/Pictures/screenshots/transcriMorse.png",
-    demoUrl: "https://piouoiq.github.io/transcriMorse/",
-    githubUrl: "https://github.com/piouoiq/transcriMorse",
-    technologies: ["HTML", "CSS", "JavaScript", "DOM"],
-    date: "November 2025",
+      "A data visualization website displaying various charts using Recharts library in a React  application.",
+    image: "src/assets/Pictures/screenshots/dataviz.png",
+    demoUrl: "https://dataviz-omega.vercel.app/",
+    githubUrl: "https://github.com/piouoiq/dataviz",
+    technologies: [
+      "React",
+      "Vite",
+      "TypeScript",
+      "TailwindsCSS",
+      "Recharts",
+      "DaisyUI",
+    ],
+    date: "December 2025",
     stared: true,
   },
   {
@@ -23,18 +30,55 @@ export const projects: Project[] = [
     githubUrl: "https://github.com/piouoiq/projet-quiz-team-mejeda",
     technologies: ["HTML", "CSS", "JavaScript", "DOM"],
     date: "October 2025",
+    stared: true,
   },
   {
-    id: "base-apparel",
-    title: "Base Apparel Home Page",
+    id: "transcrimorse",
+    title: "TranscriMorse",
     description:
-      "A coming soon home page for a clothing brand built with mobile-first approach.",
-    image: "src/assets/Pictures/screenshots/html_css_av_ex3.png",
-    demoUrl: "/exercices/Exercice-3/index.html",
+      "A website that transcript Morse-code to text and the other way around.",
+    image: "src/assets/Pictures/screenshots/transcriMorse.png",
+    demoUrl: "https://piouoiq.github.io/transcriMorse/",
+    githubUrl: "https://github.com/piouoiq/transcriMorse",
+    technologies: ["HTML", "CSS", "JavaScript", "DOM"],
+    date: "November 2025",
+    stared: true,
+  },
+
+  {
+    id: "paws-agency",
+    title: "Paw's Model Agency",
+    description:
+      "A mini portfolio for the Paw's model agency with a grid layout for displaying pictures.",
+    image: "src/assets/Pictures/screenshots/html_css_av_ex1.png",
     githubUrl:
-      "https://github.com/piouoiq/Ada_tech_School/tree/main/%5BGHN%5D%20%5BC20%5D%20HTML-CSS-avanc%C3%A9/Exercice-3",
+      "https://github.com/piouoiq/Ada_tech_School/tree/main/%5BGHN%5D%20%5BC20%5D%20HTML-CSS-avanc%C3%A9/Exercice-1",
     technologies: ["HTML", "CSS"],
     date: "October 2025",
+    stared: false,
   },
-  // Ajoute les autres projets ici...
+  {
+    id: "bonjour-javascript",
+    title: "Bonjour JavaScript",
+    description:
+      "A JavaScript greeting picker that displays 'Bonjour' or 'Bonsoir' depending on the time indicated by the user.",
+    image: "src/assets/Pictures/screenshots/bjrJS.jpg",
+    githubUrl:
+      "https://github.com/piouoiq/Ada_tech_School/tree/main/Exercices_individuels/01_bonjour_javascript.md",
+    technologies: ["HTML", "JavaScript"],
+    date: "October 2025",
+    stared: false,
+  },
+  {
+    id: "guess-a-number",
+    title: "Guess a Number",
+    description:
+      "A JavaScript mini game where the user guesses a number chosen by the machine, with hints indicating if the guess is lower, higher, or correct.",
+    image: "src/assets/Pictures/screenshots/guess_a_number.png",
+    githubUrl:
+      "https://github.com/piouoiq/Ada_tech_School/tree/main/Exercices_individuels/02_guess_a_number.md",
+    technologies: ["HTML", "JavaScript"],
+    date: "October 2025",
+    stared: false,
+  },
 ];
--- src/assets/Components/ProjectsSection.tsx
+++ src/assets/Components/ProjectsSection.tsx
@@ -2,12 +2,17 @@ import { projects } from "../../Data/Projects";
 import { ProjectCard } from "./ProjectCard";
 
 export default function ProjectsSection() {
+  const sortedProjects = [...projects].sort((a, b) => {
+    if (a.stared && !b.stared) return -1;
+    if (!a.stared && b.stared) return 1;
+    return 0;
+  });
   return (
     <section id="projects" className="py-16 ">
       <div className="container mx-auto px-4">
-        <h2 className="text-4xl font-bold text-center mb-12">My Projects</h2>
+        <h2 className="text-4xl font-bold  mb-12 underline">My Projects</h2>
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-          {projects.map((project) => (
+          {sortedProjects.map((project) => (
             <ProjectCard key={project.id} project={project} />
           ))}
         </div>
