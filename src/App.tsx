23b5754b6905d0400f5e04bada34ea899a882125
piouoiq <melargilliere@gmail.com>
2025-12-23 19:45:10 +0100

    feat structure curriculum

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router";
 import HomePage from "./assets/Pages/HomePage";
 import ProjectPage from "./assets/Pages/ProjectPage";
+import Curriculum from "./assets/Pages/curriculum";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/ProjectPage" element={<ProjectPage />} />
+        <Route path="/Curriculum" element={<Curriculum />} />
       </Routes>
     </Router>
   );
--- src/assets/Pages/curriculum.tsx
+++ src/assets/Pages/curriculum.tsx
@@ -2,6 +2,147 @@ import Header from "../Components/Header";
 
 import Footer from "../Components/Footer";
 
-export default function curriculum() {
-  return;
+export default function Curriculum() {
+  return (
+    <>
+      <Header />
+      <main
+        className=" font-gruppo font-bold bg-cover text-black"
+        style={{ backgroundImage: "url('/Pictures/background.svg')" }}
+      >
+        <div>
+          <ul className="timeline timeline-snap-icon max-md:timeline-compact timeline-vertical">
+            <li>
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-start mb-10 md:text-end">
+                <time className="font-mono italic">Sept. 2025 - June 2026</time>
+                <div className="text-lg font-black">
+                  Student web developper at Ada Tech School
+                </div>
+                <p>
+                  Learning all the basics of being a developper, from
+                  frameworks, librairies, languages to proper code structuration
+                  and ways of working.
+                </p>
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end md:mb-10">
+                <time className="font-mono italic">2024 -2025</time>
+                <div className="text-lg font-black">
+                  Self-teaching via OpenClassroom
+                </div>
+                <p>description</p>
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-start mb-10 md:text-end">
+                <time className="font-mono italic">2020 - 2022</time>
+                <div className="text-lg font-black">
+                  University studies on Korean Language
+                </div>
+                <p>description</p>
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end md:mb-10">
+                <time className="font-mono italic">2019 - 2020</time>
+                <div className="text-lg font-black">
+                  Korean languages studies in an organisation.{" "}
+                </div>
+                <p>description</p>
+              </div>
+              <hr />
+            </li>
+            <li>
+              <hr />
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-start mb-10 md:text-end">
+                <time className="font-mono italic">2013 - 2015 </time>;
+                <div className="text-lg font-black">
+                  University studies in languages (Inglish and Italian) .
+                </div>
+                <p>description</p>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
 }
