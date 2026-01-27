821be5e9007690bd683f971bfea8a13e53239349
piouoiq <melargilliere@gmail.com>
2026-01-27 14:39:03 +0100

    feat : curriculum tabs

--- src/assets/Pages/curriculum.tsx
+++ src/assets/Pages/curriculum.tsx
@@ -7,16 +7,40 @@ export default function Curriculum() {
   return (
     <>
       <Header />
-      <main
-        className=" font-gruppo font-bold bg-cover text-black"
-        style={{ backgroundImage: "url('/Pictures/background.svg')" }}
-      >
-        <div className="flex">
-          <div>
+      <main>
+        <div
+          className=" tabs tabs-box font-gruppo font-bold bg-cover text-black"
+          style={{ backgroundImage: "url('/Pictures/background.svg')" }}
+        >
+          <input
+            type="radio"
+            name="my_tabs_6"
+            className="tab hover:border-black hover:border-3"
+            aria-label="My studies"
+          />
+          <div className="tab-content border-black border-3  p-6">
             <StudyTimeline />
           </div>
-          <div>
+
+          <input
+            type="radio"
+            name="my_tabs_6"
+            className="tab hover:border-black hover:border-3"
+            aria-label="My work Experience"
+          />
+          <div className="tab-content border-black border-3 p-6">
             <WorkTimeline />
+          </div>
+
+          <input
+            type="radio"
+            name="my_tabs_6"
+            className="tab hover:border-black hover:border-3"
+            aria-label="My skills"
+            defaultChecked
+          />
+          <div className="border-black border-3 tab-content  p-6">
+            Tab content 3
           </div>
         </div>
       </main>
