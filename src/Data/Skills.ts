2b6691fccbfd8a363ce920464077011d55b8e4a5
piouoiq <melargilliere@gmail.com>
2026-01-28 15:04:56 +0100

    fix logo routes

--- src/Data/Skills.ts
+++ src/Data/Skills.ts
@@ -2,58 +2,58 @@ import type { SkillTypes } from "../Types/SkillsTypes";
 
 export const Skills: SkillTypes[] = [
   {
-    img: "public/Pictures/logos technos/HTML5.svg",
+    img: "/Pictures/logos technos/HTML5.svg",
     alt: "HTML5 logo",
     infoBubble: "HTML 5",
   },
 
   {
-    img: "public/Pictures/logos technos/CSS3.svg",
+    img: "/Pictures/logos technos/CSS3.svg",
     alt: "CSS3 logo",
     infoBubble: "CSS 3",
   },
   {
-    img: "public/Pictures/logos technos/js.png",
+    img: "/Pictures/logos technos/js.png",
     alt: "JavaScript logo",
     infoBubble: "JavaScript",
   },
   {
-    img: "public/Pictures/logos technos/Git.svg",
+    img: "/Pictures/logos technos/Git.svg",
     alt: "Git logo",
     infoBubble: "Git",
   },
   {
-    img: "public/Pictures/logos technos/GitHub.svg",
+    img: "/Pictures/logos technos/GitHub.svg",
     alt: "GitHub logo",
     infoBubble: "GitHub",
   },
   {
-    img: "public/Pictures/logos technos/Bash.svg",
+    img: "/Pictures/logos technos/Bash.svg",
     alt: "Bash logo",
     infoBubble: "Bash",
   },
   {
-    img: "public/Pictures/logos technos/Visual Studio Code (VS Code).svg",
+    img: "/Pictures/logos technos/Visual Studio Code (VS Code).svg",
     alt: "visual studio code logo",
     infoBubble: "VS Code",
   },
   {
-    img: "public/Pictures/logos technos/React.svg",
+    img: "/Pictures/logos technos/React.svg",
     alt: "React logo",
     infoBubble: "React",
   },
   {
-    img: "public/Pictures/logos technos/TypeScript.svg",
+    img: "/Pictures/logos technos/TypeScript.svg",
     alt: "TypeScript logo",
     infoBubble: "TypeScript",
   },
   {
-    img: "public/Pictures/logos technos/Tailwind CSS.svg",
+    img: "/Pictures/logos technos/Tailwind CSS.svg",
     alt: "Tailwind CSS logo",
     infoBubble: "Tailwind",
   },
   {
-    img: "public/Pictures/logos technos/MySQL.svg",
+    img: "/Pictures/logos technos/MySQL.svg",
     alt: "MySQL logo",
     infoBubble: "MySQL",
   },
