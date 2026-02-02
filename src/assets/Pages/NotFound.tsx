9406d54e7051ad113361be4d072c7109881c03b2
piouoiq <melargilliere@gmail.com>
2026-02-02 14:41:25 +0100

    feat : not found page structure

---
+++ src/assets/Pages/NotFound.tsx
@@ -0,0 +1,14 @@
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <main>
+        <h1>Page Not Found</h1>
+      </main>
+      <Footer />
+    </>
+  );
+}
