fb0d7d9f3f749632e2907fec206640480c5ee145
piouoiq <melargilliere@gmail.com>
2026-02-02 14:41:59 +0100

    feat: not pound page routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import { HashRouter as Router, Routes, Route } from "react-router";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router";
 import HomePage from "./assets/Pages/HomePage";
 import ProjectPage from "./assets/Pages/ProjectPage";
 import Curriculum from "./assets/Pages/curriculum";
+import NotFound from "./assets/Pages/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,8 @@ function App() {
         <Route path="/" element={<HomePage />} />
         <Route path="/ProjectPage" element={<ProjectPage />} />
         <Route path="/Curriculum" element={<Curriculum />} />
+        <Route path="/404" element={<NotFound />} />
+        <Route path="*" element={<Navigate to="/404" replace />} />
       </Routes>
     </Router>
   );
