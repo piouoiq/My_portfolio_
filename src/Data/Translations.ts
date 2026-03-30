9fd7934a63a1c7e9f855bf2f1418beb8e8162cd9
piouoiq <melargilliere@gmail.com>
2026-03-31 01:29:39 +0200

    feat : translations

---
+++ src/Data/Translations.ts
@@ -0,0 +1,25 @@
+export const HomePageTranslations = {
+  fr: {
+    aboutTitle: "À propos de moi",
+    alt: "Photographie de Melissa",
+    bio1: "Salut ! Je suis Melissa, une étudiante développeuse. J'apprends actuellement tout ce qu'il faut pour devenir dev. J'ai hâte de travailler sur des projets innovants et significatifs.",
+    bio2: "Sur ce portfolio, vous trouverez certains de mes projets actuels mais aussi certains que j'ai réalisés dans le passé.",
+  },
+  en: {
+    aboutTitle: "About me",
+    alt: "Melissa's photograph",
+    bio1: " Hi ! I'm Melissa, a student developer. I'm currently learning everything needed to become a dev. I can't wait to work on innovative and meaningful projects.",
+    bio2: "On this portfolio you'll find some of my current ones but also some I did in the past.",
+  },
+};
+
+export const headerTranslations = {
+  fr: {
+    name: "Melissa Largilliere",
+    title: "Étudiante développeuse à Ada Tech School",
+  },
+  en: {
+    name: "Melissa Largilliere",
+    title: "Student developer at Ada Tech School",
+  },
+};
